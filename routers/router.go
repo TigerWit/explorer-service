c1c4d1e6d19363d3dffc54ce5875cab3e3273da2
shaozhiming <zhiming.smk@gmail.com>
2019-01-11 08:31:20 +0000

    feature/20181211_prod

--- routers/router.go
+++ routers/router.go
@@ -1,11 +1,13 @@
 package routers
 
 import (
+	"explorer-service/controllers"
 	"github.com/astaxie/beego"
-	"tiger-blockchain-api/controllers"
 )
 
 func init() {
 	beego.Router("/querybykey", &controllers.MainController{}, "get:Querybykey")
 	beego.Router("/history", &controllers.MainController{}, "get:History")
+	beego.Router("/gettxidspec", &controllers.MainController{}, "get:GetTxIdSpec")
+	beego.Router("/gettxbyid", &controllers.MainController{}, "get:GetTxByID")
 }
